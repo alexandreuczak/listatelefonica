82eb95704b39530e5997aadc3bf6b1392f4227b2
Alexandre Uczak <alexandre.uczak@gmail.com>
2015-12-03 11:20:18 -0200

    script de criacao do banco

---
+++ listatelefonica.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.16  Distrib 10.1.8-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: listatelefonica
+-- ------------------------------------------------------
+-- Server version	10.1.8-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contatos`
+--
+
+DROP TABLE IF EXISTS `contatos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contatos` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `data` date NOT NULL,
+  `operadora` int(11) NOT NULL,
+  `serial` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `operadora` (`operadora`),
+  CONSTRAINT `contatos_ibfk_1` FOREIGN KEY (`operadora`) REFERENCES `operadoras` (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contatos`
+--
+
+LOCK TABLES `contatos` WRITE;
+/*!40000 ALTER TABLE `contatos` DISABLE KEYS */;
+INSERT INTO `contatos` VALUES (1,'alexandre','99999-9999','2015-12-02',1,NULL),(2,'alexandre uczak','99999-8888','2015-12-02',2,NULL),(3,'Bryan Uczak','9999-9999','2015-12-02',6,NULL),(4,'Marcia Uczak','99999-8888','2015-12-02',3,'I,1'),(5,'Bryan Uczak','88888-9999','2015-12-02',1,'\\MU6R'),(6,'Teste teste 1','1111-1111','2015-12-02',4,'RSC=M'),(7,'fdgtrtytryry','11111-1111','2015-12-02',6,'+]KFO');
+/*!40000 ALTER TABLE `contatos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `operadoras`
+--
+
+DROP TABLE IF EXISTS `operadoras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `operadoras` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `categoria` varchar(20) NOT NULL,
+  `preco` double NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `operadoras`
+--
+
+LOCK TABLES `operadoras` WRITE;
+/*!40000 ALTER TABLE `operadoras` DISABLE KEYS */;
+INSERT INTO `operadoras` VALUES (1,'OI','Celular',2),(2,'VIVO','Celular',1),(3,'TIM','Fixo',3),(4,'GVT','Fixo',4),(5,'EMBRATEL','Fixo',2),(6,'CLARO','Celular',1);
+/*!40000 ALTER TABLE `operadoras` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-03 11:18:18
