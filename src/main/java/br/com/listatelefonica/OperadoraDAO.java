d89936689cee1e80d1a5d2ac17a8c9bde9062f68
alexandreuczak <alexandre.uczak@gmail.com>
2016-10-11 22:09:07 -0300

    using query dsl

--- src/main/java/br/com/listatelefonica/OperadoraDAO.java
+++ src/main/java/br/com/listatelefonica/OperadoraDAO.java
@@ -1,9 +1,23 @@
 package br.com.listatelefonica;
 
+import java.util.List;
+
+import com.mysema.query.jpa.impl.JPAQuery;
+
 public class OperadoraDAO extends GenericDAO<Operadora>{
 
 	public OperadoraDAO() {
 		super(Operadora.class, "listatelefonica");
+	}
+	
+	@Override
+	public List<Operadora> pesquisar() {
+		QOperadora operadora = QOperadora.operadora;
+		JPAQuery query = new JPAQuery(getEntityManager());
+		
+		List<Operadora> operadoras = query.from(operadora).where(operadora.categoria.equalsIgnoreCase("fixo")).list(operadora);
+		
+		return operadoras;
 	}
 
 }
